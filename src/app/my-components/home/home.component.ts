72df3e0e168d394cd1c76a025d09686af29dd5a2
VagTsop <vatsop52@gmail.com>
2022-09-03 16:52:12 +0300

    change home screen basec on role

--- src/app/my-components/home/home.component.ts
+++ src/app/my-components/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from 'src/services/authentication.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
-  constructor() { }
+  role: any;
+  constructor(private authenticationService: AuthenticationService,) { }
   ngOnInit(): void {
+    this.getUserRole();
+  }
+
+  getUserRole() {
+    this.role = this.authenticationService.getUserFromLocalCache().role
+    console.log(this.role);
+
   }
 }
