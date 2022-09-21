4084f5577cf00a217a1bd3369ecd598e6ecd178c
VagTsop <vatsop52@gmail.com>
2022-09-21 19:27:24 +0300

    download qrcode image

--- src/app/my-components/instrument-series/instrument-series-form-popup/instrument-series-form-popup.component.ts
+++ src/app/my-components/instrument-series/instrument-series-form-popup/instrument-series-form-popup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { QRCodeElementType } from 'angularx-qrcode';
 import { InstrumentSeriesService } from 'src/services/instrument-series.service';
 import { InstrumentSeriesRequest } from 'src/transport/instrument-series.request';
 import { GenericComponent } from '../../generic.component';
@@ -27,6 +28,7 @@ export class InstrumentSeriesFormPopupComponent extends GenericComponent impleme
   showInput: boolean = true;
   showGenerateQrButton = true;
   showQrCode: boolean = false;
+  elementType = "canvas" as QRCodeElementType
 
   constructor(private instrumentSeriesService: InstrumentSeriesService,
     private dialogRef: MatDialogRef<InstrumentSeriesFormPopupComponent>,
@@ -281,6 +283,54 @@ export class InstrumentSeriesFormPopupComponent extends GenericComponent impleme
         tempSelectedTwo.add(recordId);
       }
     }
+  }
+
+  saveAsImage(parent: any) {
+    let parentElement = null
+
+    if (this.elementType === "canvas") {
+      // fetches base 64 data from canvas
+      parentElement = parent.qrcElement.nativeElement
+        .querySelector("canvas")
+        .toDataURL("image/png")
+    } else if (this.elementType === "img" || this.elementType === "url") {
+      // fetches base 64 data from image
+      // parentElement contains the base64 encoded image src
+      // you might use to store somewhere
+      parentElement = parent.qrcElement.nativeElement.querySelector("img").src
+    } else {
+      alert("Set elementType to 'canvas', 'img' or 'url'.")
+    }
+
+    if (parentElement) {
+      // converts base 64 encoded image to blobData
+      let blobData = this.convertBase64ToBlob(parentElement)
+      // saves as image
+      const blob = new Blob([blobData], { type: "image/png" })
+      const url = window.URL.createObjectURL(blob)
+      const link = document.createElement("a")
+      link.href = url
+      // name of the file
+      link.download = "Qrcode_" + this.form.controls.instrumentSeriesCode.value;
+      link.click()
+    }
+  }
+
+  private convertBase64ToBlob(Base64Image: string) {
+    // split into two parts
+    const parts = Base64Image.split(";base64,")
+    // hold the content type
+    const imageType = parts[0].split(":")[1]
+    // decode base64 string
+    const decodedData = window.atob(parts[1])
+    // create unit8array of size same as row data length
+    const uInt8Array = new Uint8Array(decodedData.length)
+    // insert all character code into uint8array
+    for (let i = 0; i < decodedData.length; ++i) {
+      uInt8Array[i] = decodedData.charCodeAt(i)
+    }
+    // return blob image after conversion
+    return new Blob([uInt8Array], { type: imageType })
   }
 
   ngOnDestroy() {
