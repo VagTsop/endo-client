b859fc4c3cf6938e64da495d3d7ded885959cf0d
VagTsop <vatsop52@gmail.com>
2022-10-08 14:30:32 +0300

    display navigation items on tablet and mobile

--- src/app/my-components/layout/header/header.component.ts
+++ src/app/my-components/layout/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
-
 import { User } from 'src/model/user';
 import { AuthenticationService } from 'src/services/authentication.service';
+import { instrumentSeriesRoutes } from '../../instrument-series/instrument-series-routing.module';
+import { instrumentRoutes } from '../../instrument/instrument-routing.module';
 import { UserDetailsPopupComponent } from '../../user/user-details-popup/user-details-popup.component';
 
 
@@ -14,15 +15,38 @@ import { UserDetailsPopupComponent } from '../../user/user-details-popup/user-de
 export class HeaderComponent implements OnInit {
   @Output() onSendIsSidebarOpened = new EventEmitter();
   isOpened: boolean = false;
+  instrumentRoutes = instrumentRoutes;
+  instrumentSeriesRoutes = instrumentSeriesRoutes;
   user: any;
   role: any = '';
+  currentRoute: any = 'Home';
+  public menuItems: any = [];
+
 
   constructor(private dialog: MatDialog, private authenticationService: AuthenticationService,
     public router: Router
   ) { }
 
   ngOnInit(): void {
-      this.authenticationService.changes.subscribe(role => this.role = role);
+    this.authenticationService.changes.subscribe(role => this.role = role);
+  }
+
+  ngDoCheck() {
+    this.getMenuItems();
+  }
+
+  getMenuItems(): Array<any> {
+    if (this.router.url.includes('user')) {
+      this.menuItems = null;
+    }
+    if (this.router.url.includes('instrument')) {
+      this.menuItems = this.instrumentRoutes;
+    }
+    if (this.router.url.includes('instrument-series')) {
+      this.menuItems = this.instrumentSeriesRoutes;
+      console.log(this.menuItems)
+    }
+    return this.menuItems;
   }
 
   onUserDetails() {
